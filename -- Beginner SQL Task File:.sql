0569a8ab4b0c4c8ac55f3fce051811608c8afabf
Courage <tobiegbude@gmail.com>
2025-10-02 09:55:14 +0100

    Ignore all .sql files except for the beginner SQL task file

--- -- Beginner SQL Task File:.sql
+++
@@ -1,9 +0,0 @@
-SELECT *,
-(case 
- when score > 750 then 'High'
- when score > 500 then 'Medium'
- else 'Low'
-End) AS Score_Category
-FROM Sales.Customers
-WHERE Score IS NOT NULL
-ORDER BY Score ASC
